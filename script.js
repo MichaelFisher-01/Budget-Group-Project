c9a5122982e4c15b4911fe020e76665563985e76
Quy Nguyen <qnguyen303@gmail.com>
2022-06-01 21:21:23 -0600

    adding description

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ var pictures3 = document.querySelector("#photo-3");
 var prices = document.querySelector("#price");
 var prices2 = document.querySelector("#price-2");
 var prices3 = document.querySelector("#price-3");
+var desc = document.querySelector("#desc");
+var desc2 = document.querySelector("#desc-2");
+var desc3 = document.querySelector("#desc-3");
 
 var time = function () {
   var date = moment().format("MMMM Do YYYY, h:mm:ss a");
@@ -26,21 +29,35 @@ function grabApi() {
     options
   )
     .then((response) => response.json())
-    .then(
-      (response) => (
-        // console.log(response.data)
-        //taking the Json data and displaying the picture of the house
-        (pictures.src =
-          response.data.home_search.results[0].primary_photo.href),
-        (pictures2.src =
-          response.data.home_search.results[1].primary_photo.href),
-        (pictures3.src =
-          response.data.home_search.results[2].primary_photo.href),
-        //taking the Json data and displaying the median
-        (prices.textContent = response.data.home_search.results[0].list_price),
-        (prices2.textContent = response.data.home_search.results[1].list_price),
-        (prices3.textContent = response.data.home_search.results[2].list_price)
-      )
+    .then((response) =>
+      console.log(response.data)
+      //taking the Json data and displaying the picture of the house
+      // (pictures.src =
+      //   response.data.home_search.results[0].primary_photo.href),
+      // (pictures2.src =
+      //   response.data.home_search.results[1].primary_photo.href),
+      // (pictures3.src =
+      //   response.data.home_search.results[2].primary_photo.href),
+      // //taking the Json data and displaying the median
+      // (prices.textContent = response.data.home_search.results[0].list_price),
+      // (prices2.textContent = response.data.home_search.results[1].list_price),
+      // (prices3.textContent = response.data.home_search.results[2].list_price),
+      // description of the house
+      // (desc.textContent =
+      //   "Beds " +
+      //   response.data.home_search.results[0].description.beds +
+      //   " Bath " +
+      //   response.data.home_search.results[0].description.baths),
+      // (desc2.textContent =
+      //   "Beds " +
+      //   response.data.home_search.results[1].description.beds +
+      //   " Bath " +
+      //   response.data.home_search.results[0].description.baths),
+      // (desc3.textContent =
+      //   "Beds " +
+      //   response.data.home_search.results[2].description.beds +
+      //   " Bath " +
+      //   response.data.home_search.results[0].description.baths)
     )
 
     .catch((err) => console.error(err));
